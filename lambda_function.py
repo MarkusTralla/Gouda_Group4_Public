a04df7fb66357c564d19c4b6740c70adee6dc89e
Markus <98045638+MarkusTralla@users.noreply.github.com>
2022-03-30 14:31:36 +0100

    Adding web scraping file
    This was the original web-scraping file used for AWS lambda, uploaded as a zip file to run cloud-based webscraping.

---
+++ lambda_function.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import json
+import urllib.request
+from urllib.request import urlopen
+import time
+
+def lambda_handler(event, context):
+    urls = [
+    'https://www.cheese.com/abbaye-de-belloc/',
+    'https://www.cheese.com/abbaye-de-belval/',
+    'https://www.cheese.com/abbaye-de-citeaux/',
+    'https://www.cheese.com/tamie/',
+    'https://www.cheese.com/abbaye-de-timadeuc/',
+    'https://www.cheese.com/abbaye-du-mont-des-cats/',
+    'https://www.cheese.com/abbots-gold/',
+    'https://www.cheese.com/abertam/',
+    'https://www.cheese.com/abondance/',
+    ]
+    
+    for url in urls:
+        page = urllib.request.Request(url)
+        response = urllib.request.urlopen(page)
+        page_html = response.read()
+        page_soup = BeautifulSoup(page_html, "html.parser")
+        for titles in page_soup.find_all('title'):
+            print(titles.text)
+        
+
